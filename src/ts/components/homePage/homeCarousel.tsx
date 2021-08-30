44cc2c52f942c535f2ec1988f5653be26c8f126b
RediChen <a0929052958@gmail.com>
2021-08-31 00:02:01 +0800

    添加暫時性的第一張輪播圖圖片

--- src/ts/components/homePage/homeCarousel.tsx
+++ src/ts/components/homePage/homeCarousel.tsx
@@ -1,4 +1,12 @@
+import React from "react";
+import img01 from "../../../assets/homeCarousel/carousel01.jpg";
 const HomeCarousel = () => {
-    return (<></>);
+    //todo 待 Carousel 元件製作完畢，再套用至此
+    const shadow = {
+        boxShadow: "0 15px 10px -5px #999"
+    } as React.CSSProperties;
+    return (
+        <img src={img01} alt="上網買盆栽，安心宅配" style={shadow} />
+    );
 }
 export default HomeCarousel;
