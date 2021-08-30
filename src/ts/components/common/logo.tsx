8be27a6e9e47d9bda6728c656d09c972e104e5e1
RediChen <a0929052958@gmail.com>
2021-08-30 20:14:24 +0800

    添加 Logo 和 購物車 的圖片

--- src/ts/components/common/logo.tsx
+++ src/ts/components/common/logo.tsx
@@ -1,5 +1,4 @@
-import logo from "../../../assets/logo/logo01.jpg"
-// const logo = require("../../assets/logo/logo01.jpg");
+import logo from "../../../assets/logo/logoH80px.png"
 export default function Logo() {
     return (
         <img src={logo} alt="2Ustyle 風格圖悠" title="2Ustyle 風格圖悠" />
