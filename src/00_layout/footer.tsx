ad2e0011bc676542728df2ab7041eac2564eed4f
RediChen <a0929052958@gmail.com>
2021-09-10 23:20:04 +0800

    將部分內容移動至不同元件
    從共同的 footer 到首頁專屬

--- src/00_layout/footer.tsx
+++ src/00_layout/footer.tsx
@@ -1,29 +1,30 @@
+import { Link } from "react-router-dom";
+
 const Footer = () =>
     <footer>
-        <h1>需要協助訂購嗎？</h1>
-        <div id="contact-box" style={{ width: "80%", display: "flex",justifyContent:"space-around" }}>
-            <div>
-                <div><img src="" alt="" /></div>
-                <div>
-                    <h2>寫email給我們</h2>
-                    <a href="mailto:service@2ustyle.com">service@2ustyle.com</a>
-                </div>
-            </div>
-            <div>
-                <div><img src="" alt="" /></div>
-                <div>
-                    <h2>撥電話給我們</h2>
-                    <a href="tel:02-2791-2816">02-2791-2816</a>
-                </div>
-            </div>
-            <div>
-                <div><img src="" alt="" /></div>
-                <div>
-                    <h2>電話服務時間</h2>
-                    <span>10:00~13:00、14:00~17:00</span>
-                </div>
-            </div>
+        <div id="footer-info-box">
+            <ul className="row-ul">
+                <li>購物說明</li>
+                <li>聯絡&nbsp;2Ustyle</li>
+                <li><a href="tel:02-2791-2816">02-2791-2816</a></li>
+            </ul>
+            <ul className="row-ul">
+                <li>網站使用條款</li>
+                <li>隱私權政策</li>
+                <li><Link to="./disclaimer">免責聲明</Link></li>
+            </ul>
+            <ul className="row-ul">
+                <li>網站維護</li>
+                <li>&copy; 2020 2Ustyle</li>
+            </ul>
         </div>
-
+        <h3 id="footer-copyright">
+            此網站僅為復刻練習，版權仍屬
+            <a href="https://www.2ustyle.com" target="_blank"
+                title="2Ustyle官方網站" rel="noreferrer">
+                官方
+            </a>
+            所有。
+        </h3>
     </footer>
 export default Footer;
--- src/01_homePage/HomePage.tsx
+++ src/01_homePage/HomePage.tsx
@@ -4,12 +4,50 @@ const HomePage = () => {
     return (
         <div>
             <HomeCarousel />
-            <ul className="row-ul">
-                <li>盆栽新品</li>
-                <li>智慧澆水</li>
-                <li>無土植栽</li>
-                <li>臉書粉專</li>
+            <ul className="row-ul" id="home-nav">
+                <li className="center">
+                    <img src="" alt="(圖)" />
+                    盆栽新品
+                </li>
+                <li className="center">
+                    <img src="" alt="(圖)" />
+                    智慧澆水
+                </li>
+                <li className="center">
+                    <img src="" alt="(圖)" />
+                    無土植栽
+                </li>
+                <li className="center">
+                    <img src="" alt="(圖)" />
+                    臉書粉專
+                </li>
             </ul>
+            <div id="home-contact-box">
+                <h1>需要協助訂購嗎？</h1>
+                <div>
+                    <div className="home-contact-card">
+                        <img src="" alt="(信)" />
+                        <div>
+                            <h2>寫email給我們</h2>
+                            <a href="mailto:service@2ustyle.com">service@2ustyle.com</a>
+                        </div>
+                    </div>
+                    <div className="home-contact-card">
+                        <img src="" alt="(筒)" />
+                        <div>
+                            <h2>撥電話給我們</h2>
+                            <a href="tel:02-2791-2816">02-2791-2816</a>
+                        </div>
+                    </div>
+                    <div className="home-contact-card">
+                        <img src="" alt="(鐘)" />
+                        <div>
+                            <h2>電話服務時間</h2>
+                            <span>10:00~13:00、14:00~17:00</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
