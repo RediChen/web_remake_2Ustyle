77df1504596dc1035e272a7b1b1ebca014a313c8
RediChen <a0929052958@gmail.com>
2021-09-11 22:04:28 +0800

    修正網址沒有帶 repo name 的問題

--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,13 @@ import Layout from './00_layout/Layout';
 import {BrowserRouter as Router} from 'react-router-dom';
 import RouteMap from './routeMap';
 import './style/style.css';
+import { env } from 'process';
+
+const basename = env.REACT_APP_BASENAME;
 
 //* BigPicture
 const App = () =>
-    <Router>
+    <Router basename={basename}>
         <Layout>
             {RouteMap}
         </Layout>
