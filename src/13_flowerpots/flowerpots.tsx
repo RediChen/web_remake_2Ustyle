af09ae9a2e47d6b1c1316d81883feb5650b3fb69
RediChen <a0929052958@gmail.com>
2021-09-16 23:50:02 +0800

    更新產品頁面的檔案位置規劃
    統一於同個資料夾，並共用一個 laytout

--- src/13_flowerpots/flowerpots.tsx
+++
@@ -1,9 +0,0 @@
-const Flower = () => {
-    return (
-        <div>
-            <h1>這裡是花的頁面～</h1>
-            <h2>準備要放很多的花。</h2>
-        </div>
-    );
-}
-export default Flower;
--- src/14_gardeningTools/gardening.tsx
+++
@@ -1,9 +0,0 @@
-const GardeningTool = () => {
-    return (
-        <div>
-            <h1>這裡是園藝用具的頁面～</h1>
-            <h2>準備要放很多的園藝用具。</h2>
-        </div>
-    );
-}
-export default GardeningTool;
