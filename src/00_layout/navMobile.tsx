525bf7e79cf0b80faa8bdedbf6d0240361ee4d2b
RediChen <a0929052958@gmail.com>
2021-09-22 23:26:10 +0800

    建立手機介面的 nav
    尚未安裝進路由

---
+++ src/00_layout/navMobile.tsx
@@ -0,0 +1,21 @@
+import { FC } from "react";
+
+const NavMobile: FC = () => {
+    return (
+        <ul id="nav-mobile">
+            <li>
+                <button><i />協助訂購</button>
+            </li>
+            <li>
+                <button><i />會員中心</button>
+            </li>
+            <li>
+                <button><i />搜尋</button>
+            </li>
+            <li>
+                <button><i />Cart 結帳</button>
+            </li>
+        </ul>
+    );
+};
+export default NavMobile;
