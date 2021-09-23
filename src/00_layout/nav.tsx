fe34f129de1780bd79f04da6bc9ae2d3fd31fa7a
RediChen <a0929052958@gmail.com>
2021-09-23 23:58:18 +0800

    建立 nav 的啟動機制
    並將 Link 改為 NavLink

--- src/00_layout/nav.tsx
+++ src/00_layout/nav.tsx
@@ -1,17 +1,24 @@
 import Logo from "src/00_commonComponents/logo";
-import { FC } from "react";
+import { CSSProperties, FC } from "react";
 import dataNav from "./data/dataNav";
 import NavTool from "./navTool";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 // Part I : Logo --> imported
 //* Part II : NavLinks */
 const NavLinks: FC = () => {
+    const activeStyle: CSSProperties = {
+        color: "#9ac448"
+    };
     const pack = dataNav.map(item =>
         <li className="center" id={item["titleEn"]}>
-            <Link to={item["href"]} className="nav-link"
+            <NavLink
+                to={item["href"]}
+                className="nav-link"
+                activeClassName="nav-link-active"
+                activeStyle={activeStyle}
                 title={item["titleZh"] + " " + item["titleEn"]}>
                 {item["titleZh"]}
-            </Link>
+            </NavLink>
         </li>
     );
     return (
