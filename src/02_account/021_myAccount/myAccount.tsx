74aa6211127f26c925d8a106a75475be5c0d2eb3
RediChen <a0929052958@gmail.com>
2021-09-24 00:17:07 +0800

    調整檔案階層規劃

---
+++ src/02_account/021_myAccount/myAccount.tsx
@@ -0,0 +1,6 @@
+import { FC } from "react";
+
+const MyAccount:FC = () => {
+    return(<></>);
+}
+export default MyAccount;
--- src/02_account/account.tsx
+++
@@ -1,6 +0,0 @@
-import { FC } from "react";
-
-const Account:FC = () => {
-    return(<></>);
-}
-export default Account;
