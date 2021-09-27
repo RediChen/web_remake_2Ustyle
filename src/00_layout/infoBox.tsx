271bec934b42835c34fcda4d7a0d6cf148037646
RediChen <a0929052958@gmail.com>
2021-09-28 00:28:04 +0800

    更新推送內容

--- src/00_layout/infoBox.tsx
+++ src/00_layout/infoBox.tsx
@@ -9,8 +9,9 @@ const InfoBox: FC = () => {
             <div id="info-box">
                 <p>
                     <i />&nbsp;
-                    【慶中秋】滿450就折50．滿1200享免運！<wbr />
-                    <Link to="">&#x21E8;&nbsp;<u>查看活動內容</u></Link>
+                    【慶中秋】滿990．就免運！（立即省120元）<wbr />
+                    &#x21E8;&nbsp;
+                    <Link to="/"><u>查看活動內容</u></Link>
                 </p>
                 <button onClick={handleClick}><i />&nbsp;我知道了！</button>
             </div >
