421605997ba74ebc06f9502c99f6a423183331cb
RediChen <a0929052958@gmail.com>
2021-09-28 10:42:24 +0800

    將 navLink 的 active 樣式從 tsx 拉到 sass

---
+++ src/00_layoutBase/nav.tsx
@@ -0,0 +1,37 @@
+import Logo from "src/00_commonComponents/logo";
+import { FC } from "react";
+import dataNav from "./data/dataNav";
+import NavTool from "./navTool";
+import { Link, NavLink } from "react-router-dom";
+// Part I : Logo --> imported
+//* Part II : NavLinks */
+const NavLinks: FC = () => {
+    const pack = dataNav.map(item =>
+        <li className="center" id={item["titleEn"]}>
+            <NavLink
+                to={item["href"]}
+                className="nav-link"
+                activeClassName="nav-link-active"
+                title={item["titleZh"] + " " + item["titleEn"]}>
+                {item["titleZh"]}
+            </NavLink>
+        </li>
+    );
+    return (
+        <ul id="nav-link-wrap" className="row-ul">{pack}</ul>
+    );
+}
+// Part III : NavTool --> imported
+//* 最後組裝 */
+const Nav: FC = () => {
+    return (
+        <nav>
+            <Link to="/">
+                <Logo />
+            </Link>
+            <NavLinks />
+            <NavTool />
+        </nav>
+    );
+}
+export default Nav;
