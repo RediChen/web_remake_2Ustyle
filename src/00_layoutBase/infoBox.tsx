10139764affe4ce0c4a35d3fd320bd3b0279d662
RediChen <a0929052958@gmail.com>
2021-09-29 20:56:17 +0800

    更新推廣訊息文字

--- src/00_layoutBase/infoBox.tsx
+++ src/00_layoutBase/infoBox.tsx
@@ -9,8 +9,8 @@ const InfoBox: FC = () => {
             <div id="info-box">
                 <p>
                     <i />&nbsp;
-                    【慶中秋】滿990．就免運！（立即省120元）<wbr />
-                    &#x21E8;&nbsp;
+                    【超值優惠】全館滿&nbsp;990&nbsp;免運．滿&nbsp;3000&nbsp;再折&nbsp;300<wbr />
+                    &nbsp;&#x21E8;&nbsp;
                     <Link to="/"><u>查看活動內容</u></Link>
                 </p>
                 <button onClick={handleClick}><i />&nbsp;我知道了！</button>
