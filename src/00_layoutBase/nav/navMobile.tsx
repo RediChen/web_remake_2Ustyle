74c39b62c7aa0df08aa1275816548946da7dcfa2
RediChen <a0929052958@gmail.com>
2021-10-14 23:28:26 +0800

    將非 PC 裝置的 nav 按鈕建立功能

--- src/00_layoutBase/nav/navMobile.tsx
+++ src/00_layoutBase/nav/navMobile.tsx
@@ -1,26 +1,33 @@
 import { FC } from "react";
+import { Link } from "react-router-dom";
 const NavMobile: FC = () => {
+    function handleCall() {
+        alert("測試版不提供電話功能。")
+    }
+    function handleSearch() {
+        alert("功能尚未建立完成...")
+    }
     return (
         <ul id="nav-mobile">
             <li>
-                <button>
+                <button onClick={handleCall}>
                     <i /><span>協助訂購</span>
                 </button>
             </li>
             <li>
-                <button>
+                <Link to='/my-account/'>
                     <i /><span>會員中心</span>
-                </button>
+                </Link>
             </li>
             <li>
-                <button>
+                <button onClick={handleSearch}>
                     <i /><span>搜尋</span>
                 </button>
             </li>
             <li>
-                <button>
+                <Link to='/cart'>
                     <i /><span>Cart 結帳</span>
-                </button>
+                </Link>
             </li>
         </ul>
     );
