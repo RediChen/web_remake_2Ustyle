ad725c870512604007a9f8cbda3a6762d251f5cf
RediChen <a0929052958@gmail.com>
2021-10-18 00:08:43 +0800

    將 listArrow 更換為 FA icon
    以利排版計算和管理，建立 chevron-tight樣式

--- src/00_commonComponents/listArrow/listArrow.tsx
+++ src/00_commonComponents/listArrow/listArrow.tsx
@@ -1,15 +1,5 @@
-import { CSSProperties, FC } from "react";
+import { FC } from "react";
 
-const ListArrow:FC = () => {
-    const style:CSSProperties = {
-        padding: "3px",
-        marginRight: "1rem",
-        display: "inline-block",
-        borderStyle: "solid",
-        borderColor: "#9ac448",
-        borderWidth: "0 3px 3px 0",
-        transform: "rotate(-45deg) translateY(-1px)"
-    };
-    return (<i style={style}></i>);
-}
+const ListArrow: FC = () =>
+    <i className="chevron-right"></i>
 export default ListArrow;
